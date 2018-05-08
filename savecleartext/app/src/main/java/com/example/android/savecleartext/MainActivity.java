cd734cc2f1655d39d4ea59ba1e9521b07bfc3bb9
sandy9999 <luckysandy99@gmail.com>
2018-05-08 13:37:24 +0530

    Final

--- savecleartext/app/src/main/java/com/example/android/savecleartext/MainActivity.java
+++ savecleartext/app/src/main/java/com/example/android/savecleartext/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.savecleartext;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,7 +13,9 @@ import com.example.android.savecleartext.R;
 
 public class MainActivity extends AppCompatActivity {
     private Button save;
+    private Button clear;
     private EditText editText;
+    String text_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +24,27 @@ public class MainActivity extends AppCompatActivity {
 
         save = (Button) findViewById(R.id.save);
         editText = (EditText) findViewById(R.id.editText);
+        clear = (Button) findViewById(R.id.clear);
+
+        SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences(getString(R.string.PREF_FILE),MODE_PRIVATE);
+        text_info = sharedPreferences.getString(getString(R.string.TEXT_INFO),"");
+        editText.setText(text_info);
     }
 
-    public void savefn(){
-        String s = editText.getText().toString();
-        editText.setText(s);
+    public void savefn(View view){
+        SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences(getString(R.string.PREF_FILE),MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(getString(R.string.TEXT_INFO),editText.getText().toString());
+        editor.commit();
+
+    }
+
+    public void clearfn(View view)
+    {
+        SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences(getString(R.string.PREF_FILE),MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.commit();
+        editText.setText("");
     }
 }
